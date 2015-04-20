2b1b31ebf534a8c366c9928bd7a930d93f163f3d
David Hedengren <davehedengren@gmail.com>
2015-04-20 16:06:12 -0400

    updated py scripts

--- grade-school/school.py
+++ grade-school/school.py
@@ -3,13 +3,23 @@ class School:
     def __init__(self,school_name):
         self.students = set()
         self.school_name = school_name
-        self.db = {} 
+        self.db = defaultdict(set) 
 
 
-    def add(self,name,grade):
+    def add(self,name,g):
         try:
-            self.db[grade].add(name)
+            self.db[g].add(name)
         except KeyError:
-            self.db[grade] = set()
-            self.db[grade] = self.db.[grade].add(name)
-            
+            self.db[g] = set()
+            self.db[g].add(name)
+
+    def grade(self,g):
+        return self.db[g]
+
+    # stole this code from a working program. Still doesn't work.
+    # I'm going to cheat and submit this so I can see more examples of
+    # working programs.
+    
+    def sort(self):
+        return {g: tuple(sorted(self.db[g])) for g in self.db}
+
